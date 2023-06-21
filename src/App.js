e33e2315105aa0e740329826677eca79a7700dfc
Shivansh mehta <112550037+ShivanshMehtaa@users.noreply.github.com>
2023-06-21 22:17:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Category from "./components/Category/Category1";
 import Login from "./components/Login/Login";
 import Signup from "./components/Login/Signup";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
